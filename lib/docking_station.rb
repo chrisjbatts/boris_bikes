0a5ddd04b1894acc1598d84f0c23bcc731314ee3
chrisjbatts <chrisjbatts@gmail.com>
2014-11-02 18:38:09 +0000

    removal of all docking station variables to bike container

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,36 +4,6 @@ class DockingStation
 
   include BikeContainer
 
-  DEFAULT_CAPACITY = 20
-
-  def initialize(options = {})
-    @bikes = []
-    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
-  end
-
-  def bike_count
-    @bikes.count
-  end
-
-  def dock(bike)
-    raise "On your bike mate. No room here." if full?
-    @bikes << bike
-  end
-
-  def release(bike)
-    @bikes.delete(bike)
-  end
-
-  def capacity
-     @bikes.count
-  end
-
-  def full?
-    bike_count == @capacity
-  end
-
-  def available_bikes
-    @bikes.reject { |bike| bike.broken? }
-  end
+   DEFAULT_CAPACITY = 20
   
 end
